011d5f971116eda0c2f1e65b129bb350683dc1b0
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-20 10:15:34 +0100

    Update docs

---
+++ docs/precache-manifest.afc18c7027ad23421b624404c0bda309.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afff07b44c6b1341423dd41597b761c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "12414984c3bd669c2f30",
+    "url": "./static/css/main.5d214fa1.chunk.css"
+  },
+  {
+    "revision": "8a49e0b8180fc54aaa1c",
+    "url": "./static/js/2.b4b1828b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.b4b1828b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12414984c3bd669c2f30",
+    "url": "./static/js/main.9df84e3d.chunk.js"
+  },
+  {
+    "revision": "03ac7a53c63309f774e9",
+    "url": "./static/js/runtime-main.24859a8c.js"
+  }
+]);
