f662ad142b2e2aad47a2b24025b2fe3c736faf83
alepareyes <alejandra.reyesg96@gmail.com>
2019-12-28 13:48:57 +0100

    add color to each type

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import '../stylesheet/pokecard.scss';
+import '../stylesheet/types.scss'
 
 function Pokemon(props) {
-  debugger
   let types = props.types;
 
   console.log(types);
@@ -14,11 +14,146 @@ function Pokemon(props) {
         <h3 className="name border--name">{props.pokemon}</h3>
         <p className="type">
           {types.map((type) => {
-            return (
-              <li className="type__item">
-                {`${type} `}
-              </li>
-            );
+
+            if (type === 'bug') {
+              return (
+                <li className="type__item type__bug" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'dark') {
+              return (
+                <li className="type__item type__dark" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'dragon') {
+              return (
+                <li className="type__item type__dragon" >
+                  {`${type} `}
+                </li>
+              );
+
+            } else if (type === 'electric') {
+              return (
+                <li className="type__item type__electric" >
+                  {`${type} `}
+                </li>
+              );
+
+            } else if (type === 'fairy') {
+              return (
+                <li className="type__item type__fairy" >
+                  {`${type} `}
+                </li>
+              );
+
+            } else if (type === 'fighting') {
+              return (
+                <li className="type__item type__fighting" >
+                  {`${type} `}
+                </li>
+              );
+
+
+
+            } else if (type === 'fire') {
+              return (
+                <li className="type__item type__fire" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'flying') {
+              return (
+                <li className="type__item type__flying" >
+                  {`${type} `}
+                </li>
+              );
+
+            } else if (type === 'ghost') {
+              return (
+                <li className="type__item type__ghost" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'grass') {
+              return (
+                <li className="type__item type__grass" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'ground') {
+              return (
+                <li className="type__item type__ground" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'ice') {
+              return (
+                <li className="type__item type__ice" >
+                  {`${type} `}
+                </li>
+              );
+
+            } else if (type === 'normal') {
+              return (
+                <li className="type__item type__normal" >
+                  {`${type} `}
+                </li>
+              );
+
+            } else if (type === 'poison') {
+              return (
+                <li className="type__item type__poison" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'psychic') {
+              return (
+                <li className="type__item type__psychic" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'rock') {
+              return (
+                <li className="type__item type__rock" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'steel') {
+              return (
+                <li className="type__item type__steel" >
+                  {`${type} `}
+                </li>
+              );
+
+
+            } else if (type === 'water') {
+              return (
+                <li className="type__item type__water" >
+                  {`${type} `}
+                </li>
+              );
+            }
+
           })}
         </p>
       </div>
